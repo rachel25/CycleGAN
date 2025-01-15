957beb7f39b200c4df4382d8a90617b425b6738f
Rachel <rchen101@jh.edu>
2025-01-14 21:27:43 -0800

    discriminator and generator class

---
+++ Discriminator.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import ConvInstanceNormLeakyReLUBlock
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels = 3, features = [64, 128, 256, 512]):
+        super().__init__()
+        self.initial_layer = nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                features[0],
+                kernel_size=4,
+                stride=1, 
+                padding=1,
+                padding_mode='reflect'
+            ),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(
+                ConvInstanceNormLeakyReLUBlock(
+                    in_channels,
+                    feature,
+                    stride = 1 if feature == feature[-1] else 2,
+                )
+            )
+            in_channels = feature
+        layers.append(
+            nn.Conv2d(
+                in_channels,
+                1,
+                kernel_size=4,
+                padding=1,
+                padding_mode='reflect',
+            )
+        )    
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.initial_layer(x)
+        return torch.sigmoid(self.model(x))
---
+++ ResidualBlock.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import ConvolutionalBlock
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels: int):
+        super().__init__()
+        self.block = nn.Sequential(
+            ConvolutionalBlock(channels, channels, add_activation = True, kernel_size = 3, padding = 1),
+            ConvolutionalBlock(channels, channels, add_activation = False, kernel_size = 3, padding = 1)
+        )
+
+    def forward(self, x):
+        return x + self.block(x)    
