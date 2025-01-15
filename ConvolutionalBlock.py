9aae99e543cba22284f0bb4e0089ca8b4d275722
Rachel Chen <rachi.chen@gmail.com>
2025-01-14 21:35:49 -0800

    add comment in convolutional block
    Summary:
    
    Test Plan:

--- ConvolutionalBlock.py
+++ ConvolutionalBlock.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 class ConvolutionalBlock(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, is_downsampling: bool = True, add_activation: bool = True, **kwargs):
         super.__init__()
-        if is_downsampling: 
+        if is_downsampling:
+            # Extract feature from sample 
             self.conv = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, padding_mode='reflect', **kwargs),
                 nn.InstanceNorm2d(out_channels),
